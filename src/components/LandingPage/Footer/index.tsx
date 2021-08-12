53d95d9edbcda3f0b2a67f47763a8d36c2591f0d
MarkLyck <mark.lyck@gmail.com>
2021-08-12 13:44:25 +0200

    :sparkles: terms and privacy policy

--- src/components/LandingPage/Footer/index.tsx
+++ src/components/LandingPage/Footer/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
-import { Space } from 'antd'
+import { Space, Typography } from 'antd'
 import styled from '@emotion/styled'
+import { useToggle } from 'ahooks'
+
 import { SocialMediaLink, Disclaimer } from 'src/ui-components'
 import { COMPANY_NAME, SOCIAL_MEDIA_LINKS } from 'src/common/constants'
+import TermsModal from 'src/components/LandingPage/Modals/Legal/Terms'
+import PrivacyPolicyModal from 'src/components/LandingPage/Modals/Legal/PrivacyPolicy'
+
+const { Link } = Typography
 
 const Logo = styled.img`
   height: 48px;
@@ -55,44 +61,59 @@ const FooterDisclaimer = styled(Disclaimer)`
   color: ${(p) => p.theme.palette.text[100]};
 `
 
-const Footer = () => (
-  <Container>
-    <Space direction="vertical" size="middle">
-      <Logo src="/logos/formula_stocks/logo_horizontal_white.svg" />
-      <Space size="middle">
-        {SOCIAL_MEDIA_LINKS.map((link) => (
-          <SocialMediaLink key={link.href} {...link} />
-        ))}
+const StyledLink = styled(Link)`
+  && {
+    text-decoration: underline;
+    color: white;
+  }
+`
+
+const Footer = () => {
+  const [termsVisible, { toggle: toggleTerms }] = useToggle()
+  const [privacyPolicyVisible, { toggle: togglePrivacyPolicy }] = useToggle()
+
+  return (
+    <Container>
+      <Space direction="vertical" size="middle">
+        <Logo src="/logos/formula_stocks/logo_horizontal_white.svg" />
+        <Space size="middle">
+          {SOCIAL_MEDIA_LINKS.map((link) => (
+            <SocialMediaLink key={link.href} {...link} />
+          ))}
+        </Space>
+        <FooterDisclaimer>
+          By accessing this website and any pages thereof, you agree to be bound by our{' '}
+          <StyledLink onClick={() => toggleTerms()}>Terms of Use</StyledLink> and{' '}
+          <StyledLink onClick={() => togglePrivacyPolicy()}>Privacy Policy</StyledLink>.
+        </FooterDisclaimer>
+        <MainDisclaimer>
+          {COMPANY_NAME} is an information provider, not an investment advisory service or a registered investment
+          advisor, does not offer individual investment advice and does not manage client funds. Unless otherwise
+          specified, all return figures shown above are for illustrative purposes only. {COMPANY_NAME} does not purport
+          to tell which securities individual customers should buy or sell for themselves and recommendations are not
+          solicitations to buy or sell a security, Like a newsletter {COMPANY_NAME} offer a model portfolio, which
+          members can choose to use as an input in their own decisionmaking process. {COMPANY_NAME} assumes no
+          responsibility or liability for your investment results. You understand and acknowledge that there is risk
+          involved in investing in securities. For technical reasons the website displays up-to-date graph data
+          refreshed daily based on backtested data. Backtested performance results have certain inherent limitations, as
+          they could potentially be designed with some benefit of hindsight, even though every effort have been made to
+          avoid such risk. Unlike an actual performance record such as the one mentioned above, backtested results do
+          not represent actual trading and may not be impacted by brokerage and other slippage fees. Also, since
+          transactions may or may not have been executed, results may have under- or over-compensated for impact, if
+          any, of certain market factors, such as lack of market liquidity or level of participation. Past results of
+          any investment system are not necessarily indicative of future results. No representation is being made that
+          you are likely to achieve profits or losses similar to those shown here. In addition, information, system
+          output, articles, and other features of our products are provided for educational and informational purposes
+          only and should not be construed as investment advice. It remains the user’s exclusive responsibility to
+          review and evaluate the content and to determine whether to accept or reject any content. {COMPANY_NAME}{' '}
+          expresses no opinion as to whether any of the website content is appropriate for a user’s investment
+          portfolio, strategy, financial situation, or investment objective(s).
+        </MainDisclaimer>
       </Space>
-      <FooterDisclaimer>
-        By accessing the Site and any pages thereof, you agree to be bound by the {COMPANY_NAME} Terms of Use and
-        Privacy Policy.
-      </FooterDisclaimer>
-      <MainDisclaimer>
-        {COMPANY_NAME} is an information provider, not an investment advisory service or a registered investment
-        advisor, does not offer individual investment advice and does not manage client funds. Unless otherwise
-        specified, all return figures shown above are for illustrative purposes only. {COMPANY_NAME} does not purport to
-        tell which securities individual customers should buy or sell for themselves and recommendations are not
-        solicitations to buy or sell a security, Like a newsletter {COMPANY_NAME} offer a model portfolio, which members
-        can choose to use as an input in their own decisionmaking process. {COMPANY_NAME} assumes no responsibility or
-        liability for your investment results. You understand and acknowledge that there is risk involved in investing
-        in securities. For technical reasons the website displays up-to-date graph data refreshed daily based on
-        backtested data. Backtested performance results have certain inherent limitations, as they could potentially be
-        designed with some benefit of hindsight, even though every effort have been made to avoid such risk. Unlike an
-        actual performance record such as the one mentioned above, backtested results do not represent actual trading
-        and may not be impacted by brokerage and other slippage fees. Also, since transactions may or may not have been{' '}
-        executed, results may have under- or over-compensated for impact, if any, of certain market factors, such as
-        lack of market liquidity or level of participation. Past results of any investment system are not necessarily
-        indicative of future results. No representation is being made that you are likely to achieve profits or losses
-        similar to those shown here. In addition, information, system output, articles, and other features of our
-        products are provided for educational and informational purposes only and should not be construed as investment
-        advice. It remains the user’s exclusive responsibility to review and evaluate the content and to determine
-        whether to accept or reject any content. {COMPANY_NAME} expresses no opinion as to whether any of the website
-        content is appropriate for a user’s investment portfolio, strategy, financial situation, or investment
-        objective(s).
-      </MainDisclaimer>
-    </Space>
-  </Container>
-)
+      <TermsModal visible={termsVisible} onClose={() => toggleTerms()} />
+      <PrivacyPolicyModal visible={privacyPolicyVisible} onClose={() => togglePrivacyPolicy()} />
+    </Container>
+  )
+}
 
 export default Footer
