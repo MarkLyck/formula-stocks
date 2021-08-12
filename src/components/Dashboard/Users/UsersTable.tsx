a568cfdb76d58ea28411e3b17ffe8f9a2a2707c3
MarkLyck <mark.lyck@gmail.com>
2021-08-12 15:29:28 +0200

    :label: fix ts

--- src/components/Dashboard/Users/UsersTable.tsx
+++ src/components/Dashboard/Users/UsersTable.tsx
@@ -97,8 +97,8 @@ const UsersTable = () => {
   const { data, loading } = useQuery(USERS_QUERY)
   const usersData = data?.usersList?.items || []
 
-  // @ts-ignore
   return (
+    // @ts-ignore
     <Table loading={loading} dataSource={usersData} columns={columns} rowKey="email" scroll={{ x: 'max-content' }} />
   )
 }
