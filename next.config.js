7330b3c492bd87559162eab2496221c0d438874b
MarkLyck <mark.lyck@gmail.com>
2021-08-14 12:41:44 +0200

    :chart_with_upwards_trend: plausible proxy

--- next.config.js
+++ next.config.js
@@ -1,16 +1,19 @@
 const withAntdLess = require('next-plugin-antd-less')
+const { withPlausibleProxy } = require('next-plausible')
 
-module.exports = withAntdLess({
-  // optional
-  modifyVars: { '@primary-color': '#3366ff' },
-  // optional
-  // lessVarsFilePath: './src/styles/variables.less',
-  // optional
-  lessVarsFilePathAppendToEndOfContent: false,
-  // optional https://github.com/webpack-contrib/css-loader#object
-  cssLoaderOptions: {},
+module.exports = withPlausibleProxy()(
+  withAntdLess({
+    // optional
+    modifyVars: { '@primary-color': '#3366ff' },
+    // optional
+    // lessVarsFilePath: './src/styles/variables.less',
+    // optional
+    lessVarsFilePathAppendToEndOfContent: false,
+    // optional https://github.com/webpack-contrib/css-loader#object
+    cssLoaderOptions: {},
 
-  webpack(config) {
-    return config
-  },
-})
+    webpack(config) {
+      return config
+    },
+  })
+)
