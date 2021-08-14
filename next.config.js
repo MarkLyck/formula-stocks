23df3afdfd981a6e480e54098f5a37ca01396aaa
MarkLyck <mark.lyck@gmail.com>
2021-08-14 12:46:50 +0200

    :fire: remove plausible script tag

--- next.config.js
+++ next.config.js
@@ -3,13 +3,8 @@ const { withPlausibleProxy } = require('next-plausible')
 
 module.exports = withPlausibleProxy()(
   withAntdLess({
-    // optional
     modifyVars: { '@primary-color': '#3366ff' },
-    // optional
-    // lessVarsFilePath: './src/styles/variables.less',
-    // optional
     lessVarsFilePathAppendToEndOfContent: false,
-    // optional https://github.com/webpack-contrib/css-loader#object
     cssLoaderOptions: {},
 
     webpack(config) {
