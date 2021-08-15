4255aeac792c1c86ac93f1f99739c336dc29aad3
MarkLyck <mark.lyck@gmail.com>
2021-08-15 10:04:32 +0200

    :chart_with_upwards_trend: move woopra to head

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -25,18 +25,6 @@ class _Document extends Document {
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap"
             rel="stylesheet"
           />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-
-          {/* CRISP live chat */}
-          <script
-            type="text/javascript"
-            dangerouslySetInnerHTML={{
-              __html: `window.$crisp=[];window.CRISP_WEBSITE_ID="661b2a78-03b3-4033-9b04-03bd9334368d";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();`,
-            }}
-          />
 
           {/* WOOPRA analytics */}
           <script
@@ -47,6 +35,18 @@ class _Document extends Document {
                   var t,i,e,n=window,o=document,a=arguments,s="script",r=["config","track","identify","visit","push","call","trackForm","trackClick"],c=function(){var t,i=this;for(i._e=[],t=0;r.length>t;t++)(function(t){i[t]=function(){return i._e.push([t].concat(Array.prototype.slice.call(arguments,0))),i}})(r[t])};for(n._w=n._w||{},t=0;a.length>t;t++)n._w[a[t]]=n[a[t]]=n[a[t]]||new c;i=o.createElement(s),i.async=1,i.src="//static.woopra.com/js/w.js",e=o.getElementsByTagName(s)[0],e.parentNode.insertBefore(i,e)
               })("woopra");
               `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+
+          {/* CRISP live chat */}
+          <script
+            type="text/javascript"
+            dangerouslySetInnerHTML={{
+              __html: `window.$crisp=[];window.CRISP_WEBSITE_ID="661b2a78-03b3-4033-9b04-03bd9334368d";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();`,
             }}
           />
         </body>
