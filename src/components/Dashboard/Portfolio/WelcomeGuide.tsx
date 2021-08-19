72c1efb01dbaa2a3538933075f2310963d07d79b
MarkLyck <mark.lyck@gmail.com>
2021-08-19 20:19:46 +0200

    :sparkles: handle tutorial dismiss

--- src/components/Dashboard/Portfolio/WelcomeGuide.tsx
+++ src/components/Dashboard/Portfolio/WelcomeGuide.tsx
@@ -1,10 +1,26 @@
+import { useState } from 'react'
 import { Guide } from 'src/ui-components'
+import useStore from 'src/lib/useStore'
+import { useMutation } from '@apollo/client'
+import { SET_TUTORIALS } from 'src/common/queries'
 
 const WelcomeGuide = () => {
+  const user = useStore((state: any) => state.user)
+  const [isVisible, setIsVisible] = useState(true)
+  const [setTutorials] = useMutation(SET_TUTORIALS)
+  console.log('🔈 ~ isVisible', isVisible)
+
+  if (!user || !isVisible) return null
+  if (user.tutorials.includes('getting-started')) return null
+
   const handleClick = () => {
     $crisp.push(['do', 'helpdesk:article:open', ['en', 'w64pew']])
   }
-  const handleClose = () => {}
+
+  const handleClose = () => {
+    setIsVisible(false)
+    setTutorials({ variables: { id: user.id, tutorials: [...user.tutorials, 'getting-started'] } })
+  }
 
   return (
     <Guide
