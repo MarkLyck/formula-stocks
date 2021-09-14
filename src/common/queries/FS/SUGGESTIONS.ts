23c801912185ce4df01c5e8480f5661807849d4c
MarkLyck <mark.lyck@gmail.com>
2021-09-14 17:25:53 +0200

    :sparkles: get latest trades and suggs

--- src/common/queries/FS/SUGGESTIONS.ts
+++ src/common/queries/FS/SUGGESTIONS.ts
@@ -1,8 +1,20 @@
 import { gql } from '@apollo/client'
 
+export const LATEST_SUGGESTION = gql`
+  query LATEST_LATEST_SUGGESTION_QUERY($planName: String) {
+    signalsList(last: 1, filter: { plan: { planID: { equals: $planName } }, type: { equals: "suggestion" } }) {
+      items {
+        date
+      }
+    }
+  }
+`
+
 export const SUGGESTIONS_QUERY = gql`
-  query SUGGESTIONS_QUERY($planName: String) {
-    signalsList(filter: { plan: { planID: { equals: $planName } }, type: { equals: "suggestion" } }) {
+  query SUGGESTIONS_QUERY($planName: String, $fromDate: Date) {
+    signalsList(
+      filter: { date: { gte: $fromDate }, plan: { planID: { equals: $planName } }, type: { equals: "suggestion" } }
+    ) {
       items {
         ticker
         action
